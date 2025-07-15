32bbaf8b04f3358ebdec0d48f2bc51aad06615d1
Overcomplicated12 <117044664+Overcomplicated12@users.noreply.github.com>
2025-07-15 03:03:29 +0000

    feat: create intake hardware interface class
    Signed-off-by: Overcomplicated12 <117044664+Overcomplicated12@users.noreply.github.com>

--- src/main/java/org/frc6423/robot/subsystems/intake/IntakeHardware.java
+++ src/main/java/org/frc6423/robot/subsystems/intake/IntakeHardware.java
@@ -2,15 +2,47 @@ package org.frc6423.robot.subsystems.intake;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 
+/**
+ * Reperesents generalized intake subsystem hardware
+ */
 public abstract class IntakeHardware {
 
-    //SETTERS
+    /**
+     * @return true if 1/3 limit switches are pressed
+     */
+    public abstract boolean noteDetected();
 
+    /**
+     * @return {@Link Rotation2d} representing the pivot motor angle
+     */
+    public abstract Rotation2d getPivotAngle();
+    
+    /**
+     * Run pivot motor with specified voltage
+     * 
+     * @param volts desired voltage output
+     */
     public abstract void setPivotVolts(double volts);
 
+    /**
+     * Run roller motor with specified voltage
+     * 
+     * @param volts desired voltage output
+     */
     public abstract void setRollerVolts(double volts);
 
+    /**
+     * Run pivot motor to specified position
+     * 
+     * @param angle {@Link Rotation2d} representin desired angle
+     */
     public abstract void setPivotAngle(Rotation2d angle);
 
+    /**
+     * Run roller motor at specified speed
+     * 
+     * @param speedRps desired speed in rotations per second
+     */
     public abstract void setRollerSpeed(double speedRps);
+    
 }
